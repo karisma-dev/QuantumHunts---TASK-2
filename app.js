de8f994339e4e2b82e435605acc9edb2a64e3898
Karisma Dev <69310505+karisma-dev@users.noreply.github.com>
2023-11-12 22:39:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+function loginWithFacebook() {
+    console.log("Logging in with Facebook");
+    displaySuccessMessage("Successfully logged in with Facebook!");
+}
+
+function loginWithGoogle() {
+    console.log("Logging in with Gmail");
+    displaySuccessMessage("Successfully logged in with Gmail!");
+}
+
+function displaySuccessMessage(message) {
+    alert(message);
+}
